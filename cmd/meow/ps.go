87d5dc84f2553f320c2cd8aa3d14511794654a8e
regy <p714140432@gmail.com>
2021-04-11 16:33:24 +0800

    chore<utils>: add utils package

--- cmd/meow/ps.go
+++ cmd/meow/ps.go
@@ -1,14 +1,14 @@
 package meow
 
 import(
-	"github.com/ReGYChang/irosh/shell"
+	"github.com/ReGYChang/irosh/utils"
 	"errors"
 )
 
 func Ps(argv []string) error {
 	if len(argv) == 2 {
-		shell.PrintPb("Mission 1",meow.mProgress[0])
-		shell.PrintPb("Mission 2",meow.mProgress[1])
+		utils.PrintPb("Mission 1",meow.mProgress[0])
+		utils.PrintPb("Mission 2",meow.mProgress[1])
 	}else{
 		return errors.New("meow: meow ps accepts no arguments.\nSee 'meow --help.'")
 	}
