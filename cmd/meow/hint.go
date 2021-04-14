f75fef35ef74fb83f9c45f15157862922ac54060
regy <p714140432@gmail.com>
2021-04-14 23:15:01 +0800

    fix: fixed minor bugs

--- cmd/meow/hint.go
+++ cmd/meow/hint.go
@@ -1,8 +1,14 @@
 package meow
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func hint() error{
+	if meow == nil{
+		return errors.New("\nERROR: meow process have not started yet.\nPlease start the process before checking progress.\n")
+	}
 	fmt.Println(meow.mHint[meow.mCurrent])
 	return nil
 }
--- cmd/meow/show.go
+++ cmd/meow/show.go
@@ -1,9 +1,14 @@
 package meow
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func show()error {
-	if meow.mCurrent != 2{
+	if meow == nil{
+		return errors.New("\nERROR: meow process have not started yet.\nPlease start the process before checking progress.\n")
+	}else if meow.mCurrent != 2{
 		fmt.Println("nothing")
 	}else{
 		fmt.Println("surprise!!")
